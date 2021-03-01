b10f8930dba558c194f6632db184b76fa7a82c60
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-01 15:01:39 -0500

    increase max time limit

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,9 +5,16 @@ it('gets 7', () => {
   recurse(
     () => cy.task('randomNumber'),
     (n) => n === 7,
+  )
+})
+
+it('gets 7 after 50 iterations or 30 seconds', () => {
+  recurse(
+    () => cy.task('randomNumber'),
+    (n) => n === 7,
     {
       limit: 50, // max number of iterations
-      timeout: 3000, // time limit in ms
+      timeout: 30000, // time limit in ms
     },
   )
 })
