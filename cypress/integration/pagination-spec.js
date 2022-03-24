26f1f30c28fd5a89886cde79e05c598c526b8704
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-24 10:28:09 -0400

    pagination example

--- cypress/integration/pagination-spec.js
+++ cypress/integration/pagination-spec.js
@@ -3,11 +3,15 @@ import { recurse } from '../..'
 describe('Table', () => {
   it('pagination', () => {
     cy.visit('/cypress/integration/pagination-sort-filter-manager')
+    // click on the "next" button
+    // until the button becomes disabled
     recurse(
       () => cy.get('[value=next]'),
+      // check if the button is disabled using jQuery ".attr()" method
+      // https://api.jquery.com/attr/
       ($button) => $button.attr('disabled') === 'disabled',
       {
-        log: 'Last page',
+        log: 'The last page',
         delay: 500, // wait half a second between clicks
         post() {
           // if the button is not disabled, click it
