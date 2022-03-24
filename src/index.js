6af04ee7ea74b89a8ff827535302d51b7be0a7e1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-24 14:08:14 -0400

    fix: refactoring code (#86)
    * start to rework post calls
    
    * tests should pass
    
    * remove comments
    
    * add tests how many times the reduce was called
    
    * longer timeout

--- src/index.js
+++ src/index.js
@@ -67,7 +67,9 @@ const RecurseDefaults = {
 
     const timeRemaining = options.started + options.timeout - now
     if (!Cypress._.isFinite(timeRemaining)) {
-      throw new Error(`timeRemaining must be a number, was ${timeRemaining}`)
+      throw new Error(
+        `timeRemaining must be a number, was ${timeRemaining}`,
+      )
     }
 
     function getErrorDetails() {
@@ -209,29 +211,36 @@ const RecurseDefaults = {
           })
         }
 
-        const newAccumulator = options.reduce(options.reduceFrom, x)
-        if (typeof newAccumulator !== 'undefined') {
-          options.reduceFrom = newAccumulator
-        }
+        const reduceStep =
+          typeof options.reduce === 'function'
+            ? cy.then(() => {
+                const newAccumulator = options.reduce(
+                  options.reduceFrom,
+                  x,
+                )
+                if (typeof newAccumulator !== 'undefined') {
+                  options.reduceFrom = newAccumulator
+                }
+              })
+            : cy
 
-        const delayStep =
-          options.delay > 0 ? cy.wait(options.delay, { log: logCommands }) : cy
-
-        const callPost = () => {
-          const result = options.post({
-            limit: options.limit,
-            value: x,
-            reduced: options.reduceFrom,
+        return reduceStep
+          .then(() => {
+            if (options.delay > 0) {
+              cy.wait(options.delay, { log: logCommands })
+            }
           })
-          return Cypress.isCy(result) ? result : cy
-        }
-
-        const postStep =
-          typeof options.post === 'function'
-            ? delayStep.then(callPost)
-            : delayStep
-
-        return postStep.then(nextIteration)
+          .then(() => {
+            if (typeof options.post === 'function') {
+              const result = options.post({
+                limit: options.limit,
+                value: x,
+                reduced: options.reduceFrom,
+              })
+              return Cypress.isCy(result) ? result : cy
+            }
+          })
+          .then(nextIteration)
       },
     )
   })
