9cc27b49133784909e38c441c2c1f269d6841300
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-23 07:54:14 -0500

    feat: add log data object (#119)
    * feat: add log data object
    
    * update another test

--- cypress/e2e/log-spec.js
+++ cypress/e2e/log-spec.js
@@ -29,14 +29,48 @@ describe('log option', () => {
 
     // verify the calls to our log function
     cy.get('@log').should('have.been.calledThrice')
-    cy.get('@log').its('firstCall.args').should('deep.equal', [1])
-    cy.get('@log').its('secondCall.args').should('deep.equal', [2])
-    cy.get('@log').its('thirdCall.args').should('deep.equal', [3])
+    cy.get('@log')
+      .its('firstCall.args')
+      .should('have.length', 2)
+      .its(0)
+      .should('equal', 1)
+    cy.get('@log')
+      .its('secondCall.args')
+      .should('have.length', 2)
+      .its(0)
+      .should('equal', 2)
+    cy.get('@log')
+      .its('thirdCall.args')
+      .should('have.length', 2)
+      .its(0)
+      .should('equal', 3)
   })
 
   it('can be a function that uses cy.log', () => {
     recurse(getTo(3), (x) => x === 3, {
       log: (k) => cy.log(`k = **${k}**`),
     })
+  })
+
+  it('passes a second parameter with details', () => {
+    const logValues = cy.stub().as('logValues')
+    const logInfo = cy.stub().as('logInfo')
+
+    recurse(getTo(3), (x) => x === 3, {
+      log(x, info) {
+        logValues(x)
+        logInfo(info)
+      },
+    })
+    cy.get('@logInfo').should('have.been.calledThrice')
+    cy.get('@logInfo')
+      .its('firstCall.args.0')
+      .should('deep.include', { iteration: 1, value: 1, limit: 20 })
+    cy.get('@logInfo')
+      .its('secondCall.args.0')
+      .should('deep.include', { iteration: 2, value: 2, limit: 19 })
+    cy.get('@logInfo')
+      .its('thirdCall.args.0')
+      .should('deep.include', { iteration: 3, value: 3, limit: 18 })
   })
 })
