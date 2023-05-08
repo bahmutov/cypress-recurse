64cc1d3e405f93d38384d8d9e65ce8bc353a352c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-08 17:49:05 -0400

    move plugins code into config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,18 @@ export default defineConfig({
   fixturesFolder: false,
   projectId: 'tbtscx',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      on('task', {
+        randomNumber() {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              const n = parseInt(Math.random().toString().slice(6, 7))
+              console.log('returning %d', n)
+              resolve(n)
+            }, 1000)
+          })
+        },
+      })
     },
     supportFile: false,
     specPattern: 'cypress/e2e/**/*spec.{js,ts}',
