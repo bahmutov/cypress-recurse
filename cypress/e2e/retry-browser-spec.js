ea8a3d569c41e4f3cc7291d2734ed0b09041ea58
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-11 08:31:12 -0400

    feat: user log function (#173)
    * feat: add user log function
    
    * confirm calls
    
    * check extract before using

--- cypress/e2e/retry-browser-spec.js
+++ cypress/e2e/retry-browser-spec.js
@@ -1,4 +1,7 @@
+/// <reference types="cypress" />
 import { sleep, retry } from '../../src/retry'
+
+chai.config.truncateThreshold = 200
 
 async function randomNumber() {
   await sleep(100)
@@ -11,6 +14,69 @@ it('retries', () => {
     retry(randomNumber, (n) => n === 7, { limit: 100, log: true }),
     { timeout: 60_000 },
   ).should('equal', 7)
+})
+
+function getTo(n, delay = 100) {
+  let k = 0
+  return async () => {
+    if (k > n) {
+      throw new Error(`Limit ${n} exceeded`)
+    }
+    await sleep(delay)
+    k += 1
+    return k
+  }
+}
+
+it('logs using my function', async () => {
+  const logSpy = Cypress.sinon.spy(console, 'log')
+  const log = ({ attempt, limit, value, successful }) => {
+    console.log(
+      'attempt %d of %d, value %o success: %o',
+      attempt,
+      limit,
+      value,
+      successful,
+    )
+  }
+
+  const x = await retry(getTo(3), (n) => n === 3, { limit: 5, log })
+  expect(x, 'final value').to.equal(3)
+  expect(logSpy, 'console.log').to.be.calledThrice
+})
+
+it('log is called correctly', async () => {
+  const log = Cypress.sinon.stub()
+  const x = await retry(getTo(3, 500), (n) => n === 3, {
+    limit: 5,
+    log,
+  })
+  expect(x, 'final value').to.equal(3)
+  expect(log, 'user log').to.be.calledThrice
+  expect(log.firstCall.args).to.deep.equal([
+    {
+      attempt: 1,
+      limit: 5,
+      value: 1,
+      successful: false,
+    },
+  ])
+  expect(log.secondCall.args).to.deep.equal([
+    {
+      attempt: 2,
+      limit: 5,
+      value: 2,
+      successful: false,
+    },
+  ])
+  expect(log.thirdCall.args).to.deep.equal([
+    {
+      attempt: 3,
+      limit: 5,
+      value: 3,
+      successful: true,
+    },
+  ])
 })
 
 async function randomList() {
--- src/retry.js
+++ src/retry.js
@@ -25,18 +25,30 @@ async function retry(fn, predicate, options = DEFAULT_RETRY_OPTIONS) {
   const result = await fn()
   const satisfied = Boolean(predicate(result))
 
-  if (mergedOptions.log) {
+  const limit = mergedOptions.attempts + mergedOptions.limit - 1
+  if (mergedOptions.log === true) {
     console.log(
       'attempt %d of %d was %s',
       mergedOptions.attempts,
-      mergedOptions.attempts + mergedOptions.limit - 1,
+      limit,
       satisfied ? '✅' : '🚨',
     )
+  } else if (typeof mergedOptions.log === 'function') {
+    mergedOptions.log({
+      attempt: mergedOptions.attempts,
+      limit,
+      value: result,
+      successful: satisfied,
+    })
   }
 
   if (satisfied) {
-    const finalResult = await options.extract(result)
-    return finalResult
+    if (typeof options.extract === 'function') {
+      const finalResult = await options.extract(result)
+      return finalResult
+    } else {
+      return result
+    }
   }
 
   if (mergedOptions.limit === 1) {
