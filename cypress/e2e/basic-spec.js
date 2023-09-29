8206a3623ca2c4571ecee139be98ec362ac971b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-09-29 14:18:28 -0400

    upgrade to Cypress v13

--- cypress/e2e/basic-spec.js
+++ cypress/e2e/basic-spec.js
@@ -1,7 +1,6 @@
 // @ts-check
 /// <reference path="../../src/index.d.ts" />
 import { recurse } from '../..'
-import 'cypress-command-chain'
 
 describe('recurse', () => {
   // since the default timeout is short
@@ -14,16 +13,20 @@ describe('recurse', () => {
   })
 
   // there is a chance that this function fails, so allow retrying it
-  it('gets 7 after 50 iterations or 60 seconds', { retries: 5 }, () => {
-    recurse(
-      () => cy.task('randomNumber'),
-      // https://github.com/bahmutov/cypress-recurse/issues/76
-      // @ts-ignore
-      (n) => n === 7,
-      {
-        limit: 50, // max number of iterations
-        timeout: 60_000, // time limit in ms
-      },
-    )
-  })
+  it(
+    'gets 7 after 50 iterations or 60 seconds',
+    { retries: 5 },
+    () => {
+      recurse(
+        () => cy.task('randomNumber'),
+        // https://github.com/bahmutov/cypress-recurse/issues/76
+        // @ts-ignore
+        (n) => n === 7,
+        {
+          limit: 50, // max number of iterations
+          timeout: 60_000, // time limit in ms
+        },
+      )
+    },
+  )
 })
